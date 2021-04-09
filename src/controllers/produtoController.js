965bb4960710fda37cf30339b2d7223ea59ae77f
pokernicolas <nicolasa7x@hotmail.com>
2021-04-09 16:35:19 -0300

    Backend com SelectAll e SelectDial

--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -27,3 +27,28 @@ exports.Insert = (req, res, next) => {
         //catch = registra o que queremos que aconteca quando a Promise falhar
         .catch(error => next(error));
 };
+
+exports.SelectAll = (req, res, next) => {
+    Produto.findAll()
+        .then(produto => {
+            if (produto) {
+                res.status(status.OK).send(produto);
+            }
+        })
+        .catch(error => next(error));
+}
+ 
+exports.SelectDetail = (req, res, next) => {
+    const id = req.params.id;
+ 
+    Produto.findByPk(id)
+        .then(produto => {
+            if (produto) {
+                res.status(status.OK).send(produto);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,6 @@ const ProdutoController = require('../controllers/produtoController.js');
 const router = express.Router();
 
 router.post('/produto', ProdutoController.Insert);
-
+router.get('/produto', ProdutoController.SelectAll);
+router.get('/produto/:id', ProdutoController.SelectDetail);
 module.exports = router;
